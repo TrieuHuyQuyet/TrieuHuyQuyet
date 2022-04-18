7a03e050fe71846aaece2a73a04811a851d1adb8
TrieuHuyQuyet <quyettrieuhuy@gmail.com>
2022-04-18 18:34:49 +0700

    baitap1804

---
+++ baithuchanh1804/src/hinhhoc.java
@@ -0,0 +1,18 @@
+public class 54
+
+    public final float PI 
+
+    public string ten:
+    public float Chuvi;
+    public float Dientich;
+    public float Thetich;
+
+    public void xuatTen() 
+        symtem.out.println("\n\==== " + ten +" =====");
+    }
+    public void inChuVi()
+        symtem.out.println("Chu vi=" + chuVi);
+    public void inDienTich()
+        symtem.out.println("Diện tích=" + dienTich);
+    public void inTheTich()
+        symtem.out.println("Thể tích=" + theTich);
