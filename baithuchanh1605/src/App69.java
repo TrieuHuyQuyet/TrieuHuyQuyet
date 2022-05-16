04f14d709f4ecfcbeb0a29f9d6b5a81f61648fc9
TrieuHuyQuyet <quyettrieuhuy@gmail.com>
2022-05-16 21:22:23 +0700

    baithuchanh1605

--- baithuchanh1605/src/App69.java
+++ baithuchanh1605/src/App69.java
@@ -16,4 +16,19 @@ public class App69 {
         Symtem.out.println(treeSetInteger);
         Symtem.out.println("Nhập phần tử cần thêm: ");
         number = scanner.nextInt();
-        if
+        if (!treeSetInteger.contains(number)) {
+            treeSetInteger.add(number);
+            Symtem.out.println("thêm thành công");
+            Symtem.out.println("các phần tử có trong treesetinteger là: " + treeSetInteger);
+            number = sc.nextInt();
+            if(!treesetinteger.contains(number)) {
+                treesetinteger.add(number);
+                System.out.println("them thanh cong");
+                System.out.println("cac phan tu co trong treesetinteger la: "+treesetinteger);
+            }
+            else {
+                System.out.println("phan tu da ton tai");
+            }
+        }
+    }
+}
